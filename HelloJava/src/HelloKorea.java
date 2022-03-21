24bbeed6b524faaf0cb336ff63509140da5cb9ef
dosunggil <chothegenius@gmail.com>
2022-03-21 12:46:08 +0900

    0321

---
+++ HelloJava/src/HelloKorea.java
@@ -0,0 +1,21 @@
+import java.security.DomainCombiner;
+
+/*
+ * 지금부터 자바 코드를 작성합니다.
+ * 
+ * 여기는 주석영역입니다.
+ * 
+ * 자바의 소스코드는 반드시 public class 클래스명 형식으로 시작합니다.
+ * 클래스명 첫 글자는 반드시 대문자이어야 합니다.
+ * 이후에는 소문자,숫자,_(언더바)의 조합으로 만듭니다.
+ * 
+ * Camel Case 명명법
+ * 두 개 이상의 단어를 조합하여 만드는 것이 권장되며
+ * 새로운 단어가 시작되는 곳에서는 대문자로 시작합니다.
+ * 
+ */
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println("우리나라만세");
+	}
+}
---
+++ HelloKorea.java
@@ -0,0 +1,13 @@
+/*
+comment
+*/
+
+// or
+
+
+public class HelloKorea {
+	// comment
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+	}
+}
