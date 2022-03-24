e404f7c22a6e807eb43d45d4c1ad0c1f590b4b55
dosunggil <chothegenius@gmail.com>
2022-03-24 11:51:50 +0900

    0324

---
+++ Java_10_Varraible_02/src/com/cho/var/Var_01.java
@@ -0,0 +1,21 @@
+package com.cho.var;
+
+public class Var_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실습입니다.
+		
+		int intNum1 = 0;
+		int intNum2 = 0;
+		
+		intNum1 = 55;
+		intNum2 = 33;
+		
+		System.out.println(intNum1 + intNum2);
+		System.out.println(intNum1 - intNum2);
+		System.out.println(intNum1 * intNum2);
+		System.out.println(intNum1 / intNum2);
+	}
+
+}
---
+++ Java_10_Varraible_02/src/com/cho/var/Var_06.java
@@ -0,0 +1,29 @@
+package com.cho.var;
+
+public class Var_06 {
+	public static void main(String[] args) {
+		
+		int intNum1=0;
+		int intNum2=0;
+		
+		int intSum =0;
+		int intDefference=0;
+		int intProduct=0;
+		int intDivide=0;
+		int intMod = 0;
+		
+		intNum1 = 89;
+		intNum2 = 3;
+		
+		intSum = intNum1 + intNum2;
+		intDefference = intNum1 - intNum2;
+		intProduct = intNum1*intNum2;
+		intDivide = intNum1/intNum2;
+		intMod = intNum1 % intNum2; //나머지
+		
+		System.out.println(intDefference);
+		
+		
+	}
+
+}
