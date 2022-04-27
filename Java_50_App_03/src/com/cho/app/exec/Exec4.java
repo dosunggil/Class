da1139ae6dfbabbdec1b8e82297742ca8ab52520
dosunggil <chothegenius@gmail.com>
2022-04-27 10:25:11 +0900

    Ex4

--- Java_50_App_03/src/com/cho/app/exec/Exec4.java
+++ Java_50_App_03/src/com/cho/app/exec/Exec4.java
@@ -8,17 +8,16 @@ public class Exec4 {
 	public static void main(String[] args) {
 		
 		int num = (int)(Math.random()*100)+1;
-		
+		int result = 0;
 		for(int i = 2 ; i < num ; i ++) {
 			if((num%i) == 0) {
-				System.out.println(num + " 은(는) 소수가 아닙니다");
-				break;
-			} else {
-				System.out.println(num + " 은(는) 소수입니다 ㅋ");
-				break;
+				result ++;
 			}
-		
 		}
-		
+		if(result != 0) {
+			System.out.println(num + " 은(는) 소수가 아닙니다 ㅋ");
+		} else {
+			System.out.println(num + " 은(는) 소수입니다 ㅋ");
+		}
 	}
 }
