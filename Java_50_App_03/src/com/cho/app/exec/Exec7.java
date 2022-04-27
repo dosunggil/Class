f6650f3133b2258f1268869c024f1c481bdbf47d
dosunggil <chothegenius@gmail.com>
2022-04-27 14:14:14 +0900

    Ex7 수정

--- Java_50_App_03/src/com/cho/app/exec/Exec7.java
+++ Java_50_App_03/src/com/cho/app/exec/Exec7.java
@@ -4,24 +4,31 @@ public class Exec7 {
 
 	public static void main(String[] args) {
 		int[] intArr = new int[100];
-		int intScore = 0;
 		int intNum = 0;
 
 		for (int i = 0; i < intArr.length; i++) {
-			intArr[i] = (int) (Math.random() * 100) + 1;
+			 intArr[i] = (int) (Math.random() * 100) + 2;
+			//intArr[i] = i + 1;
 		}
-		
+
+		/*
+		 * for (int i : intArr) { System.out.println(i); }
+		 */
+
 		for (int i = 0; i < intArr.length; i++) {
+
+			if (intArr[i] == 1)
+				continue;
 
 			for (int j = 2; j < intArr[i]; j++) {
 				if ((intArr[i] % j) == 0) {
 					intNum++;
 					break;
-				} //end if
+				} // end if
 			} // end for j
 
 		} // end for i
-		
-		System.out.println(100-intNum);
+
+		System.out.println("배열 intArr 에는 " + (100 - intNum) + "개의 소수가 있습니다.");
 	}
 }
