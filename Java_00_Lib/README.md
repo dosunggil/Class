7428335399676306e57b583d04d9927106187de8
dosunggil <chothegenius@gmail.com>
2022-05-04 09:31:31 +0900

    utils 프로젝트 적용해보기

---
+++ Java_00_Lib/README.md
@@ -0,0 +1,7 @@
+# 2022-05-04
+
+* 프로젝트 우클릭 export -> java의 jar -> 브라우즈 눌러서 c드라이브의 jdk_3rd 에 저장
+
+### 새로운 프로젝트에 적용하려면 프로젝트 우클릭 -> build path -> configure build path -> Classpath 누르고 add External JARs..
+
+* 이제 프로젝트마다 Line 클래스를 만들지 않아도 적용할 수 있다.
---
+++ Java_60_App_01/src/com/cho/app/LineEx1.java
@@ -0,0 +1,14 @@
+package com.cho.app;
+
+import com.cho.utils.Line;
+
+public class LineEx1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Line.dLine(50));
+		System.out.println("나의 라이브러리");
+		System.out.println(Line.sLine(50));
+	}
+	
+}
